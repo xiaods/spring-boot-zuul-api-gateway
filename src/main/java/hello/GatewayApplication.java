a6f2d2b60341119b77d8d6b50eaa9cf2902bb015
Deshi Xiao <xiaods@gmail.com>
2018-02-11 01:45:09 +0800

    add discoveryclient

--- src/main/java/hello/GatewayApplication.java
+++ src/main/java/hello/GatewayApplication.java
@@ -2,12 +2,14 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import hello.filters.pre.SimpleFilter;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
   public static void main(String[] args) {
